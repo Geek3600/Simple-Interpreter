55665d9d8643d62383e60a0878eec208304678f0
Geek3600 <2741057934@qq.com>
2025-03-26 16:40:39 +0800

    finish part 2

--- src/main/scala/Interpreter.scala
+++ src/main/scala/Interpreter.scala
@@ -26,56 +26,74 @@ class Interpreter(line:String)
     var pos: Int = 0
     val text: String = line
     var current_token: Token = null
+    var current_char: Char = this.text(this.pos)
     
-    def error() = {
-        throw new Exception("Error parsing input")
+    // 遇到不支持的token时抛出异常
+    def unknown_error() = {
+        throw new Exception("Unknown token in parsing input")
     }
 
-    // 识别并获取下一个token
+    // 扫描下一个字符
+    def scan_next_position() = {
+        this.pos += 1 // 指向下一个字符
+        if (this.pos > this.text.length() - 1) { // 如果已经到了句子的末尾
+            this.current_char = '\u0000'
+        }
+        else { // 如果没有到了句子的末尾
+            this.current_char = this.text(this.pos)
+        } 
+
+    }
+
+    // def skip_whitespace() = { // 跳过空格
+    //     if (this.current_char == ' ' && this.current_char != null) {
+    //         this.scan_next_position()
+    //     }
+    // }
+    def find_integer(): String = {
+        var integer: String = ""
+        while (this.current_char != '\u0000' && this.current_char.isDigit) {
+            integer = integer + this.current_char
+            this.scan_next_position()
+        }
+        return integer
+    }
+
+    // 词法分析器
     def get_next_token(): Token = {
-        
-        // 保存整个表达式
-        var _text:String = this.text
 
-        // 如果已经到了句子的末尾，返回EOF
-        if (this.pos > this.text.length() - 1)  {
-            return new Token(TOKENS_TYPE.EOF, null)
+        // 循环是为了跳过任意个空格
+        while (this.current_char != '\u0000') {
+            if (current_char.isDigit) {     // 如果当前字符是数字，则返回一个整数token
+                return new Token(TOKENS_TYPE.INTEGER, this.find_integer())
+            }
+            else if (current_char == '+') {  // 如果当前字符是加号，则返回一个加号token
+                val temp_token = new Token(TOKENS_TYPE.PLUS, "+")
+                this.scan_next_position()
+                return temp_token
+            }
+            else if (current_char == '-') {  // 如果当前字符是减号，则返回一个减号token
+                val temp_token = new Token(TOKENS_TYPE.SUB, "-")
+                this.scan_next_position()
+                return temp_token
+            }
+            else if (current_char == ' ') {  // 如果当前字符是空格，直接跳过
+                this.scan_next_position()
+            }
+            else {
+                this.unknown_error()
+            }
         }
-
-        // 保存当前指向的字符
-        val current_char:Char = _text(this.pos)
-
-        // 如果当前字符是数字，则返回一个整数token
-        if (current_char.isDigit) {
-            val temp_token = new Token(TOKENS_TYPE.INTEGER, current_char.toString())
-            this.pos += 1
-            return temp_token
-        }
-        else if (current_char == '+') {  // 如果当前字符是加号，则返回一个加号token
-            val temp_token = new Token(TOKENS_TYPE.PLUS, "+")
-            this.pos += 1
-            return temp_token
-        }
-        else if (current_char == '-') {  // 如果当前字符是减号，则返回一个减号token
-            val temp_token = new Token(TOKENS_TYPE.SUB, "-")
-            this.pos += 1
-            return temp_token
-        }
-        else if (current_char == ' ') {  // 如果当前字符是空格，直接跳过
-            this.pos += 1
-            return temp_token
-        }
-
-        this.error()
+        return new Token(TOKENS_TYPE.EOF, "")
     }
 
-    // 保存获取的token
+    // 判断当前token的类型符合预期，并且获取下一个token
     def eat(token_type: String) = {
         if (this.current_token._type == token_type) {
             this.current_token = this.get_next_token()
         }
         else {
-            this.error()
+            this.unknown_error()
         }
     }
 
@@ -93,7 +111,12 @@ class Interpreter(line:String)
         // 获取加号
         val op:Token = this.current_token
         //  println(op.str())
-        this.eat(TOKENS_TYPE.PLUS)
+        if (op._type == TOKENS_TYPE.PLUS) {
+            this.eat(TOKENS_TYPE.PLUS)
+        }
+        else {
+            this.eat(TOKENS_TYPE.SUB)
+        }
 
         // 获取右边第二个整数
         val right:Token  = this.current_token
@@ -101,7 +124,12 @@ class Interpreter(line:String)
         this.eat(TOKENS_TYPE.INTEGER)   
 
         // 相加
-        left._value.toInt + right._value.toInt
+        if (op._type == TOKENS_TYPE.PLUS) {
+            left._value.toInt + right._value.toInt
+        }
+        else {
+            left._value.toInt - right._value.toInt
+        }
     }
 }
 
--- src/test/scala/InterpreterTest.scala
+++ src/test/scala/InterpreterTest.scala
@@ -7,15 +7,23 @@ import Interpreter._
 class AdderSpec extends AnyFlatSpec 
 {
     behavior of "Adder"
-    val numSample = 100000
+    val numSample = 1000
     it should "Random Test" in {
         for (i <- 1 to numSample) {
-            val a = Random.nextInt(10)
-            val b = Random.nextInt(10)
-            val sum = a + b
+            val a = Random.nextInt(99999)
+            val b = Random.nextInt(999999)
+            val op_select = Random.nextInt(2) 
+            val op = if (op_select == 0) "+" else "-"
 
-            info(s"$i/$numSample: $a + $b = $sum")
-            val interpreter = new Interpreter(s"$a+$b")
+            var sum = 0
+            if (op_select == 0)  {
+                sum = a + b
+
+            }
+            else  sum = a - b
+
+            info(s"$i/$numSample: $a $op $b = $sum")
+            val interpreter = new Interpreter(s"$a$op$b")
             assert(interpreter.expr() == sum)
         }
     }
